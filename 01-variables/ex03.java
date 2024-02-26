fb267e9b00ec38f25a84828129733de326ab15a1
Willian <78714158+Ishidawg@users.noreply.github.com>
2024-02-26 16:02:35 -0300

    ex03.java

---
+++ 01-variables/ex03.java
@@ -0,0 +1,14 @@
+class Ex03 {
+    public static void main(String[] args) {
+        
+        //Name variables an give it a type
+        String nomeDoAluno = "Willian";
+        int idadeDoAluno = 19;
+        double notaDaProva1;
+        double notaDaPorva2;
+        double mediaFinal;
+        
+        //Test
+        System.out.println(nomeDoAluno + " tem " + idadeDoAluno + " anos.");
+    }
+}
