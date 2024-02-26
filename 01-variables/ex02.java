d1f35ba517a0c4ca140fb3110060a63ab25691bf
Willian <78714158+Ishidawg@users.noreply.github.com>
2024-02-26 15:57:36 -0300

    ex02.java

---
+++ 01-variables/ex02.java
@@ -0,0 +1,18 @@
+class Ex02 {
+    public static void main(String[] args) {
+        
+        //Fix variables sintax Camelcase
+        String umNomeProduto;
+        double precoProdutoPorcentagem;
+        String marcaDoProduto;
+        int quantidadeProduto;
+        String descricaoProduto;
+        String nomeProduto;
+        int numeroProduto;
+        double precoDoProduto;
+        int estoqueDisponivel;
+        int totalDeProdutos;
+        
+        System.out.println("teste");
+    }
+}
