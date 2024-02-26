33e6fb20e8dff4e5c2e12f8a589051b14dcabb6d
Willian <78714158+Ishidawg@users.noreply.github.com>
2024-02-26 16:45:54 -0300

    ex07.java

---
+++ 01-variables/ex07.java
@@ -0,0 +1,20 @@
+public class ControleEstoque {
+    public static void main(String[] args) {
+     
+        String nomeProduto = "Fone de Ouvido";
+        double precoProduto = 29.99;
+        int quantidadeProduto = 10;
+       
+       
+        // Sim, possui marca, positivo, afirmativo
+        boolean marcaProduto;
+        marcaProduto = true;
+
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Nome: " + nomeProduto);
+        System.out.println("Marca: " + marcaProduto);
+        System.out.println("Preco em reais: " + precoProduto);
+        System.out.println("Quantidade em estoque: " + quantidadeProduto);
+    }
+}
