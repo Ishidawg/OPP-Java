da6ab81d4171e694da68963581ce02b498051144
Willian <78714158+Ishidawg@users.noreply.github.com>
2024-02-26 16:41:19 -0300

    ex06.java

---
+++ 01-variables/ex06.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        int numeroProduto = 123; 
+        double precoProduto = 25.00; 
+        String descricaoProduto = "Fone de ouvido Bluetooth";
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Numero: " + numeroProduto);
+        System.out.println("Preco em reais: " + precoProduto);
+        System.out.println("Descricao: " + descricaoProduto);
+    }
+}
