4055d81f08e59e43256b1e8ebe15a4113cb5db2e
Willian <78714158+Ishidawg@users.noreply.github.com>
2024-03-09 16:28:12 -0300

    Update ex04.java

--- input-dataProcessing-output/ex04.java
+++ input-dataProcessing-output/ex04.java
@@ -1,32 +1,28 @@
 import java.util.Scanner;
 
 class HelloWorld {
-    static void shit(double a, double b) {
-        double sum = a + b;
-        double sub = a - b;
-        double mult = a * b;
-        double div = a / b;
-        
-        System.out.println("Sum: " + sum);
-        System.out.println("Sub: " + sub);
-        System.out.println("mult: " + mult);
-        System.out.println("div: " + div);
-    }
-    
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
         
-        double firstNum = 0.0;
-        double secNum = 0.0;
+        double notaUm = 0.0;
+        double notaDois = 0.0;
+        double notaTres = 0.0;
+        double notaQuatro = 0.0;
         
-        System.out.println("First number: ");
-        firstNum = scanner.nextDouble();
-        System.out.println("Second number: ");
-        secNum = scanner.nextDouble();
+        System.out.println("Informe a primeira nota: ");
+        notaUm = scanner.nextDouble();
+        System.out.println("Informe o segundo valor: ");
+        notaDois = scanner.nextDouble();
+        System.out.println("Informe a terceira nota: ");
+        notaTres = scanner.nextDouble();
+        System.out.println("Informe a quarta nota: ");
+        notaQuatro = scanner.nextDouble();
         
         scanner.close();
         
-        shit(firstNum, secNum);
+        double media = (notaUm + notaDois + notaTres + notaQuatro) / 4;
+        
+        System.out.println("Mehdia igual a: " + media);
     }
 }
