fc94ff8925f0f0db99a923b6da75f9dbb29085fc
Willian <78714158+Ishidawg@users.noreply.github.com>
2024-04-14 19:17:27 -0300

    help...

--- 05-for-if-obj/exercises/src/Atividade5.java
+++ 05-for-if-obj/exercises/src/Atividade5.java
@@ -1,2 +1,70 @@
-public class Atividade5 {
+class Car {
+    public String color;
+    public int doors;
+    public int horsepower;
+    public int age;
+
+    public Car(String color, int doors, int horsepower, int age) {
+        this.color = color;
+        this.doors = doors;
+        this.horsepower = horsepower;
+        this.age = age;
+    }
+
+    public void drive() {
+        System.out.println("The car is driving.");
+    }
+
+    public void drift() {
+        System.out.println("The car is drifting!");
+    }
+
+    public void shiftGear() {
+        System.out.println("Gear shifted.");
+    }
+
+    public void gas() {
+        System.out.println("Gas pedal pressed.");
+    }
+
+    public void brake() {
+        System.out.println("Brake pedal pressed.");
+    }
 }
+
+class TestCar {
+    Car car;
+
+    public TestCar(Car car) {
+        this.car = car;
+    }
+
+    public void driveTenMiles() {
+        System.out.println("Driving for ten miles...");
+        car.drive();
+    }
+
+    public void brakeAtFifty() {
+        System.out.println("Approaching 50 mph. Preparing to brake...");
+        car.brake();
+    }
+}
+
+public class Atividade5 {
+    public static void main(String[] args) {
+        Car myCar = new Car("Red", 4, 300, 5);
+
+        TestCar test = new TestCar(myCar);
+
+        test.driveTenMiles();
+
+        test.brakeAtFifty();
+        
+        System.out.println("Let's have some fun!");
+        myCar.drift();
+        myCar.shiftGear();
+        myCar.gas();
+    }
+}
+
+
